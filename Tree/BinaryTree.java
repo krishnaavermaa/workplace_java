4661132d34003cdaa6577ce6728c8fa1556bda31
krishna <krishna.1923en1078@kiet.edu>
2022-03-01 19:59:35 +0530

    BinaryTree.java created

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.LinkedList;
 import java.util.Queue;
 
 
@@ -27,21 +28,21 @@ public class BinaryTree {
 		Queue <Node>q;
 		System.out.print("Enter root element ");
 		root=new Node(sc.nextInt());
-		q=new Queue<>(root);
+		q=new LinkedList<>();
 		Node temp;
 		q.offer(root);
 		while(!q.isEmpty())
 		{
 			temp=q.poll();
 			int data;
-			System.out.print("Enter left element of "+temp.data+" or -1");
+			System.out.print("\nEnter left element of "+temp.data+" or -1 ");
 			data=sc.nextInt();
 			if(data!=-1)
 			{
 				temp.left=new Node(data);
 				q.offer(temp.left);
 			}
-			System.out.print("Enter right element of "+temp.data+" or -1");
+			System.out.print("\nEnter right element of "+temp.data+" or -1 ");
 			data=sc.nextInt();
 			if(data!=-1)
 			{
